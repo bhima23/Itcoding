de6d17e5cea5e4c45edd355a86a5b6cd47dea765
bhima23 <156943637+bhima23@users.noreply.github.com>
2024-01-17 23:12:13 +0530

    IT -information

---
+++ first.js
@@ -0,0 +1,45 @@
+name ="bhima dasur dfdsf";
+console.log(name)
+age = 4545;
+console.log(age+33);
+
+let name1="bhima dasur ";
+console.log(name1);
+
+const name2= "prachi ";
+console.log(name2);
+
+ let student={
+    name:'bhima dasur',
+    age:45,
+    mark:5.22,
+    result:true,
+
+
+
+ };
+ student["age"]+33;
+
+ const product={
+   name:"beautproduct",
+   rating:4,
+   offer:787,
+
+
+ };
+  console.log(product);
+
+
+
+  const profile ={
+    username:"bhima dasur ",
+   isfollow:true,
+   isfollow:false,
+   followers:"155k",
+   following:"464564k"
+
+
+
+  };
+  console.log(profile)
+ 
